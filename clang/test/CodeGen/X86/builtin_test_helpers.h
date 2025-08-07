eb9cdcffb823e44a1236836948b708409d8c9f5c
Bhasawut Singhaphan <bhasawut@gmail.com>
2025-08-07 20:54:16 +0700

    [Headers][X86] Allow AVX512 _mm512_set* intrinsics to be used in constexpr
    This PR adds constexpr support for the following AVX512F/BW/FP16 set intrinsics:
    
    ```
    _mm512_set1_pd
    _mm512_set1_ps
    _mm512_set1_epi8
    _mm512_set1_epi16
    _mm512_set1_epi32
    _mm512_set1_epi64
    
    _mm512_set4_pd
    _mm512_set4_ps
    _mm512_set4_epi32
    _mm512_set4_epi64
    
    _mm512_set_pd
    _mm512_set_ps
    _mm512_set_epi8
    _mm512_set_epi16
    _mm512_set_epi32
    _mm512_set_epi64
    
    _mm512_setr_pd
    _mm512_setr_ps
    _mm512_setr_epi8
    _mm512_setr_epi16
    _mm512_setr_epi32
    _mm512_setr_epi64
    
    _mm512_set1_ph
    _mm512_set_ph
    _mm512_setr_ph
    
    _mm_setzero_ph
    _mm256_setzero_ph
    _mm512_setzero_ph
    ```
    
    Closes https://github.com/llvm/llvm-project/issues/152288.
    Part of https://github.com/llvm/llvm-project/issues/30794.

--- clang/test/CodeGen/X86/builtin_test_helpers.h
+++ clang/test/CodeGen/X86/builtin_test_helpers.h
@@ -122,6 +122,36 @@ constexpr bool match_v16si(__m512i _v, int a, int b, int c, int d, int e, int f,
   return v[0] == a && v[1] == b && v[2] == c && v[3] == d && v[4] == e && v[5] == f && v[6] == g && v[7] == h && v[8] == i && v[9] == j && v[10] == k && v[11] == l && v[12] == m && v[13] == n && v[14] == o && v[15] == p;
 }
 
+constexpr bool match_v32hi(__m512i _v, short __b00, short __b01, short __b02, short __b03, short __b04, short __b05, short __b06, short __b07,
+                                       short __b08, short __b09, short __b10, short __b11, short __b12, short __b13, short __b14, short __b15,
+                                       short __b16, short __b17, short __b18, short __b19, short __b20, short __b21, short __b22, short __b23,
+                                       short __b24, short __b25, short __b26, short __b27, short __b28, short __b29, short __b30, short __b31) {
+  __v32hi v = (__v32hi)_v;
+  return v[ 0] == __b00 && v[ 1] == __b01 && v[ 2] == __b02 && v[ 3] == __b03 && v[ 4] == __b04 && v[ 5] == __b05 && v[ 6] == __b06 && v[ 7] ==  __b07 &&
+         v[ 8] == __b08 && v[ 9] == __b09 && v[10] == __b10 && v[11] == __b11 && v[12] == __b12 && v[13] == __b13 && v[14] == __b14 && v[15] ==  __b15 &&
+         v[16] == __b16 && v[17] == __b17 && v[18] == __b18 && v[19] == __b19 && v[20] == __b20 && v[21] == __b21 && v[22] == __b22 && v[23] ==  __b23 &&
+         v[24] == __b24 && v[25] == __b25 && v[26] == __b26 && v[27] == __b27 && v[28] == __b28 && v[29] == __b29 && v[30] == __b30 && v[31] ==  __b31;
+}
+
+constexpr bool match_v64qi(__m512i _v, char __b00, char __b01, char __b02, char __b03, char __b04, char __b05, char __b06, char __b07,
+                                       char __b08, char __b09, char __b10, char __b11, char __b12, char __b13, char __b14, char __b15,
+                                       char __b16, char __b17, char __b18, char __b19, char __b20, char __b21, char __b22, char __b23,
+                                       char __b24, char __b25, char __b26, char __b27, char __b28, char __b29, char __b30, char __b31,
+                                       char __b32, char __b33, char __b34, char __b35, char __b36, char __b37, char __b38, char __b39,
+                                       char __b40, char __b41, char __b42, char __b43, char __b44, char __b45, char __b46, char __b47,
+                                       char __b48, char __b49, char __b50, char __b51, char __b52, char __b53, char __b54, char __b55,
+                                       char __b56, char __b57, char __b58, char __b59, char __b60, char __b61, char __b62, char __b63) {
+  __v64qi v = (__v64qi)_v;
+  return v[ 0] == __b00 && v[ 1] == __b01 && v[ 2] == __b02 && v[ 3] == __b03 && v[ 4] == __b04 && v[ 5] == __b05 && v[ 6] == __b06 && v[ 7] ==  __b07 &&
+         v[ 8] == __b08 && v[ 9] == __b09 && v[10] == __b10 && v[11] == __b11 && v[12] == __b12 && v[13] == __b13 && v[14] == __b14 && v[15] ==  __b15 &&
+         v[16] == __b16 && v[17] == __b17 && v[18] == __b18 && v[19] == __b19 && v[20] == __b20 && v[21] == __b21 && v[22] == __b22 && v[23] ==  __b23 &&
+         v[24] == __b24 && v[25] == __b25 && v[26] == __b26 && v[27] == __b27 && v[28] == __b28 && v[29] == __b29 && v[30] == __b30 && v[31] ==  __b31 &&
+         v[32] == __b32 && v[33] == __b33 && v[34] == __b34 && v[35] == __b35 && v[36] == __b36 && v[37] == __b37 && v[38] == __b38 && v[39] ==  __b39 &&
+         v[40] == __b40 && v[41] == __b41 && v[42] == __b42 && v[43] == __b43 && v[44] == __b44 && v[45] == __b45 && v[46] == __b46 && v[47] ==  __b47 &&
+         v[48] == __b48 && v[49] == __b49 && v[50] == __b50 && v[51] == __b51 && v[52] == __b52 && v[53] == __b53 && v[54] == __b54 && v[55] ==  __b55 &&
+         v[56] == __b56 && v[57] == __b57 && v[58] == __b58 && v[59] == __b59 && v[60] == __b60 && v[61] == __b61 && v[62] == __b62 && v[63] ==  __b63;
+}
+
 #define TEST_CONSTEXPR(...) static_assert(__VA_ARGS__)
 
 #else
